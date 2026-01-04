8cfab4e0d85e66c6588ed3923e03891a9a1238ac
Harsh21777 <harshbalasojadhav8@gmail.com>
2026-01-04 12:30:53 +0530

    stage-10:CONSOLE OUPUT

---
+++ batch.sh
@@ -0,0 +1,6 @@
+load --os /home/expos/myexpos/spl/spl_progs/os_startup.xsm
+load --init /home/expos/myexpos/expl/expl_progs/squares.xsm
+load --int=10 /home/expos/myexpos/spl/spl_progs/haltProg.xsm
+load --exhandler /home/expos/myexpos/spl/spl_progs/haltProg.xsm
+load --int=timer /home/expos/myexpos/spl/spl_progs/sample_timer.xsm
+load --int=7 /home/expos/myexpos/spl/spl_progs/sample_int7.xsm
