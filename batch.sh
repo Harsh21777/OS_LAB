432257c72cad96770af2f9a187bcf0a2f1aca543
Harsh21777 <harshbalasojadhav8@gmail.com>
2026-01-06 12:57:37 +0530

    stage-12:MULTIPROGRAMMING

--- batch.sh
+++ batch.sh
@@ -1,5 +1,7 @@
 load --os /home/expos/myexpos/spl/spl_progs/os_startup.xsm
-load --init /home/expos/myexpos/expl/expl_progs/squares.xsm
+load --library /home/expos/myexpos/expl/library.lib
+load --init /home/expos/myexpos/expl/samples/ass12_q1_init.xsm
+load --idle /home/expos/myexpos/expl/samples/ass12_q1_idle.xsm
 load --int=10 /home/expos/myexpos/spl/spl_progs/haltProg.xsm
 load --exhandler /home/expos/myexpos/spl/spl_progs/haltProg.xsm
 load --int=timer /home/expos/myexpos/spl/spl_progs/sample_timer.xsm
