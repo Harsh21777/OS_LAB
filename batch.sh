2cbcc24ba15b9711c5b9f52582471426c51159ae
Harsh21777 <harshbalasojadhav8@gmail.com>
2026-01-13 15:14:31 +0530

    stage-14:RR Scheduler

--- batch.sh
+++ batch.sh
@@ -1,9 +1,11 @@
 load --os /home/expos/myexpos/spl/spl_progs/os_startup.xsm
+load --idle /home/expos/myexpos/expl/samples/idle.xsm
+load --init /home/expos/myexpos/expl/samples/odd100.xsm
+load --exec /home/expos/myexpos/expl/samples/even100.xsm
 load --module 7 /home/expos/myexpos/spl/spl_progs/boot_module.xsm
+load --module 5 /home/expos/myexpos/spl/spl_progs/scheduler_module.xsm
 load --library /home/expos/myexpos/expl/library.lib
-load --init /home/expos/myexpos/expl/samples/ass12_q1_init.xsm
-load --idle /home/expos/myexpos/expl/samples/ass12_q1_idle.xsm
-load --int=10 /home/expos/myexpos/spl/spl_progs/haltProg.xsm
+load --int=10 /home/expos/myexpos/spl/spl_progs/int_10.xsm
 load --exhandler /home/expos/myexpos/spl/spl_progs/haltProg.xsm
 load --int=timer /home/expos/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=7 /home/expos/myexpos/spl/spl_progs/sample_int7.xsm
