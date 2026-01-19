d32915b548dd6304418973bdb42e79bf694449af
Harsh21777 <harshbalasojadhav8@gmail.com>
2026-01-19 09:50:42 +0530

    stage-16:CONSOLE INPUT

--- batch.sh
+++ batch.sh
@@ -1,15 +1,17 @@
 load --os /home/expos/myexpos/spl/spl_progs/os_startup.xsm
 load --idle /home/expos/myexpos/expl/samples/idle.xsm
-load --init /home/expos/myexpos/expl/samples/odd100.xsm
-load --exec /home/expos/myexpos/expl/samples/even100.xsm
+load --init /home/expos/myexpos/expl/samples/gcd.xsm
 load --module 7 /home/expos/myexpos/spl/spl_progs/boot_module.xsm
 load --module 5 /home/expos/myexpos/spl/spl_progs/scheduler_module.xsm
 load --library /home/expos/myexpos/expl/library.lib
 load --int=10 /home/expos/myexpos/spl/spl_progs/int_10.xsm
+load --int=6 /home/expos/myexpos/spl/spl_progs/int_6.xsm
+load --int=console /home/expos/myexpos/spl/spl_progs/console.xsm
 load --exhandler /home/expos/myexpos/spl/spl_progs/haltProg.xsm
 load --int=timer /home/expos/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=7 /home/expos/myexpos/spl/spl_progs/sample_int7.xsm
 load --module 0 /home/expos/myexpos/spl/spl_progs/resourceManager_module0.xsm
 load --module 4 /home/expos/myexpos/spl/spl_progs/deviceManager_module4.xsm
+
 
 
